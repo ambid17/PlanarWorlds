fc9d6fcf4eb8fe20581760afff35627368e53b0d
Sabien <ambid17@gmail.com>
2021-07-22 01:49:13 -0500

    generate parent colliders

--- Assets/Scripts/PrefabSystem/PrefabManager.cs
+++ Assets/Scripts/PrefabSystem/PrefabManager.cs
@@ -47,7 +47,41 @@ public class PrefabManager : StaticMonoBehaviour<PrefabManager>
         }
 
         PlaceObjectInContext(instance);
+        CreateObjectCollider(instance);
         _prefabGizmoManager.OnTargetObjectChanged(instance);
+    }
+
+    // If the object doesn't have a collider
+    // Create a parent collider, and make it encapsulate all it's child meshes
+    private void CreateObjectCollider(GameObject instance)
+    {
+        BoxCollider myCollider = instance.GetComponent<BoxCollider>();
+        if (!myCollider)
+        {
+            myCollider = instance.AddComponent<BoxCollider>();
+        }
+
+        Bounds bounds = new Bounds(instance.transform.position, Vector3.zero);
+        Renderer myRenderer = instance.GetComponent<Renderer>();
+        if (myRenderer)
+        {
+            bounds.Encapsulate(myRenderer.bounds);
+        }
+
+        myCollider.center = bounds.center - instance.transform.position;
+        myCollider.size = bounds.size;
+
+        Transform[] children = instance.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            Renderer childRenderer = child.GetComponent<Renderer>();
+            if (childRenderer)
+            {
+                bounds.Encapsulate(childRenderer.bounds);
+            }
+            myCollider.center = bounds.center - instance.transform.position;
+            myCollider.size = bounds.size;
+        }
     }
 
     private void PlaceObjectInContext(GameObject instance)
